3a2fd262f2645702f3f5407bd0840d9c94dfb9c9
262882 <2628829@students.wits.ac.za>
2023-04-08 08:14:42 +0200

    Define image type, add directory

--- add_features.py
+++ add_features.py
@@ -8,6 +8,12 @@ import glob
 import uuid
 import numpy as np
 
+output_dir1 = "processed"
+try:
+    os.makedirs(output_dir1)
+except FileExistsError:
+    pass
+
 img_list = glob.glob(os.path.join('./',"*.json"))
 prefix = str(uuid.uuid4().fields[-1])[:5]
 
@@ -17,7 +23,7 @@ for count, name in enumerate(img_list):
     with open(name, 'r') as input_file:
         img_dict = json.load(input_file)
         
-    img_dict["type"]="drill"
+    img_dict["type"]="match" # ["drill", "match"]
 
     with open("./processed/" + name[2:], 'w') as out_file:
         json.dump(img_dict, out_file)
